87ebdbf5ecdae78c8eff0750bab3b6f79b4fff3d
Antonis Kyriakopoulos <antonkyriako@gmail.com>
2025-03-04 11:04:12 +0200

    bugfix and example

---
+++ backend/example.py
@@ -0,0 +1,50 @@
+import geometrics as gmt
+import foilgeneral as flg
+import randsectgen as rsg
+import domain as dm
+import automesher
+
+# read ordinates
+afld = flg.read_ord()
+
+# reformat all intput ordinates
+_ = list(afld.keys())
+
+# tidy them up in an array
+namearr = [[_[6], _[1], _[0]], [_[8], _[7], _[3]], [_[2], _[4], _[5]]]
+
+# select airfoil
+name = namearr[1][1]
+afl = afld[name]
+
+# select le and te devices
+le_func = rsg.bare_le
+te_func = rsg.fowler_1slot
+
+# generate le and te geometries, and merge them into a single airfoil geometry
+aflcrv = flg.foilpatch(le_func(afl), te_func(afl))
+
+# generate GeoShape objects for geometry
+gsl = gmt.crv2gs(aflcrv)
+
+# merge objects into one
+gs = gmt.gs_merge(gsl)
+
+# sort elements
+gs = dm.element_sort(gs)
+
+# prepare section for meshing
+gs = dm.simple_section_prep(gs, 10, 1.001, 1.5)
+# generate boundary layers
+outers = gs.simple_boundary_layer_gen(1, 50)
+# generate trail
+gs.simple_trailpoint_gen(300, 300, 1500, 1, 3, 30)
+# generate control volume
+gs.simple_controlvol_gen(outers, 1500, 500, 40)
+
+# print the domain data and plot it locally
+print(gs)
+gs.plot()
+
+# generate mesh
+automesher.mesh(gs, name, 3)
